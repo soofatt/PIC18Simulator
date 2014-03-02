795947d86801ecb0e9f5f0c26956dd6530fc5fb9
Soo Fatt <soofatt93@gmail.com>
2014-03-03 00:44:53 +0800

    Add test for comf(using GPR bank)

--- src/Comf.c
+++ src/Comf.c
@@ -7,7 +7,11 @@ char FSR[0x1000];
 void comf(Bytecode *code){
 	if(code->operand2 == 0)
 		FSR[WREG] = ~(FSR[code->operand1]);
-	if(code->operand2 == 1)
-		FSR[code->operand1] = ~(FSR[code->operand1]);
-
+	if(code->operand2 == 1){
+		if(code->operand3 == 0)
+			FSR[code->operand1] = ~(FSR[code->operand1]);
+		else if(code->operand3 == 1)
+			FSR[code->operand1+(FSR[BSR]<<8)] = ~(FSR[code->operand1+(FSR[BSR]<<8)]);
+	}
+	
 }
--- test/test_Comf.c
+++ test/test_Comf.c
@@ -5,7 +5,7 @@
 void setUp(){}
 void tearDown(){}
 
-void test_comf_should_complement_value_in_a_file_register(){
+void test_comf_should_complement_value_in_a_file_register_in_ACCESS_bank(){
 	//Test fixture
 	Bytecode code = {.instruction = {.mnemonic = COMF, .name = "comf"},
 					 .operand1 = 0x055, 
@@ -18,5 +18,24 @@ void test_comf_should_complement_value_in_a_file_register(){
 	comf(&code);
 	
 	TEST_ASSERT_EQUAL_HEX8(0x5A, FSR[code.operand1]);
+	
+}
+
+void test_comf_should_complement_value_in_a_file_register_GPR_bank(){
+	//Test fixture
+	Bytecode code = {.instruction = {.mnemonic = COMF, .name = "comf"},
+					 .operand1 = 0x035, 
+					 .operand2 = 1, 
+					 .operand3 = 1					
+					};
+				
+	//Initialize FSR[0xA35] to 0xF0
+	FSR[BSR] = 0xA;
+	FSR[code.operand1+(FSR[BSR]<<8)] = 0xF0;
+	comf(&code);
+	
+	//printf("File register: %#x\n", code.operand1+(FSR[BSR]<<8));
+	
+	TEST_ASSERT_EQUAL_HEX8(0x0F, FSR[code.operand1+(FSR[BSR]<<8)]);
 	
 }
