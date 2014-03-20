463e6f58c8d318260a8dd09dd8d057150fdcb46c
Soo Fatt <soofatt93@gmail.com>
2014-03-21 01:46:45 +0800

    Add test for invalid operand2 and invalid operand3

--- src/Comf.c
+++ src/Comf.c
@@ -9,6 +9,12 @@ void comf(Bytecode *code){
 	if(code->operand1 < 0x00 || code->operand1 > 0xFF){
 		Throw(ERR_INVALID_OPERAND);
 	}
+	if(code->operand2 < -5 || code->operand2 > 1){
+		Throw(ERR_INVALID_OPERAND);
+	}
+	if(code->operand3 < -5 || code->operand3 > 1){
+		Throw(ERR_INVALID_OPERAND);
+	}
 	else{
 		if(code->operand2 == 0){
 			if(code->operand3 == 0)
--- test/test_Comf.c
+++ test/test_Comf.c
@@ -7,7 +7,6 @@ void setUp(){}
 void tearDown(){}
 
 void test_comf_should_complement_value_in_a_file_register_in_ACCESS_bank(){
-	int catchError;
 	//Test fixture
 	Bytecode code = {.instruction = {.mnemonic = COMF, .name = "comf"},
 					 .operand1 = 0x055, 
@@ -17,20 +16,14 @@ void test_comf_should_complement_value_in_a_file_register_in_ACCESS_bank(){
 				
 	//Initialize FSR[0x055] to 0xA5
 	FSR[code.operand1] = 0xA5;
+
+	comf(&code);
 	
-	Try{
-		comf(&code);
-	} Catch(catchError){
-		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND, catchError);
-		return;
-	}
-	
-	TEST_ASSERT_EQUAL_HEX8(0x5A, FSR[code.operand1]);
+	TEST_ASSERT_EQUAL_HEX8(0x5A, FSR[0x055]);
 	
 }
 
 void test_comf_should_complement_value_in_a_file_register_in_GPR_bank(){
-	int catchError;
 	//Test fixture
 	Bytecode code = {.instruction = {.mnemonic = COMF, .name = "comf"},
 					 .operand1 = 0x035, 
@@ -41,22 +34,16 @@ void test_comf_should_complement_value_in_a_file_register_in_GPR_bank(){
 	//Initialize FSR[0xA35] to 0xF0
 	FSR[BSR] = 0xA;
 	FSR[code.operand1+(FSR[BSR]<<8)] = 0xF0;
-	
-	Try{
-		comf(&code);
-	} Catch(catchError){
-		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND, catchError);
-		return;
-	}
-	
+
+	comf(&code);
+
 	//printf("File register: %#x\n", code.operand1+(FSR[BSR]<<8));
 	
-	TEST_ASSERT_EQUAL_HEX8(0x0F, FSR[code.operand1+(FSR[BSR]<<8)]);
+	TEST_ASSERT_EQUAL_HEX8(0x0F, FSR[0xA35]);
 	
 }
 
 void test_comf_should_complement_value_in_a_file_register_in_ACCESS_bank_and_store_in_WREG(){
-	int catchError;
 	//Test fixture
 	Bytecode code = {{.mnemonic = COMF, .name = "comf"},
 							.operand1 = 0x08A, 
@@ -67,19 +54,13 @@ void test_comf_should_complement_value_in_a_file_register_in_ACCESS_bank_and_sto
 	//Initialize FSR[0x08A] to 0xFF
 	FSR[code.operand1] = 0xFF;
 	
-	Try{
-		comf(&code);
-	} Catch(catchError){
-		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND, catchError);
-		return;
-	}
+	comf(&code);
 	
 	TEST_ASSERT_EQUAL_HEX8(0x00, FSR[WREG]);
 
 }
 
 void test_comf_should_complement_value_in_a_file_register_in_GPR_bank_and_store_in_WREG(){
-	int catchError;
 	//Test fixture
 	Bytecode code = {{.mnemonic = COMF, .name = "comf"},
 							.operand1 = 0x04B, 
@@ -91,18 +72,13 @@ void test_comf_should_complement_value_in_a_file_register_in_GPR_bank_and_store_
 	FSR[BSR] = 0x2;
 	FSR[code.operand1+(FSR[BSR]<<8)] = 0x63;
 	
-	Try{
-		comf(&code);
-	} Catch(catchError){
-		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND, catchError);
-		return;
-	}
+	comf(&code);
 	
 	TEST_ASSERT_EQUAL_HEX8(0x9C, FSR[WREG]);
 
 }
 
-void test_comf_should_throw_exception_if_invalid_operand(){
+void test_comf_should_throw_exception_if_invalid_operand1(){
 	int catchError;
 	//Test fixture
 	Bytecode code = {{.mnemonic = COMF, .name = "comf"},
@@ -117,8 +93,38 @@ void test_comf_should_throw_exception_if_invalid_operand(){
 		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND, catchError);
 		return;
 	}
-	
-	TEST_ASSERT_EQUAL_HEX8(0x00, FSR[WREG]);
+}
 
+void test_comf_should_throw_exception_if_invalid_operand2(){
+	int catchError;
+	//Test fixture
+	Bytecode code = {{.mnemonic = COMF, .name = "comf"},
+							.operand1 = -1, 
+							.operand2 =	-8, 
+							.operand3 = 0					
+					};			
 	
+	Try{
+		comf(&code);
+	} Catch(catchError){
+		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND, catchError);
+		return;
+	}
+}
+
+void test_comf_should_throw_exception_if_invalid_operand3(){
+	int catchError;
+	//Test fixture
+	Bytecode code = {{.mnemonic = COMF, .name = "comf"},
+							.operand1 = -1, 
+							.operand2 =	1, 
+							.operand3 = 5					
+					};			
+	
+	Try{
+		comf(&code);
+	} Catch(catchError){
+		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND, catchError);
+		return;
+	}
 }
